820c4689b2069dd15cf53bee66db9f6638b0f8c5
Michal Moudrý <moudry.michal@live.com>
2025-11-24 20:04:10 +0100

    progressed through some of lang tour

--- first_program/bin/main.ml
+++ first_program/bin/main.ml
@@ -1,1 +1,14 @@
-let () = print_endline "Hello, World!"
+let square x = x * x
+
+let cat a b = a ^ " " ^ b
+
+let rec range lo hi =
+  if lo > hi then []
+  else lo :: range (lo + 1) hi
+
+let () =
+  square 50 |> string_of_int |> print_endline;
+  cat "Hello," "World!" |> print_endline;
+  print_endline "Hello, World!";
+  List.iter (Printf.printf "%d") (range 2 5);
+  print_newline()
