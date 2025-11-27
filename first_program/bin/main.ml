1f9178200415e566ac2944e7d343f7ad3a5bd22f
Michal Moudrý <moudry.michal@live.com>
2025-11-27 22:26:41 +0100

    updated main.ml

--- first_program/bin/main.ml
+++ first_program/bin/main.ml
@@ -6,9 +6,31 @@ let rec range lo hi =
   if lo > hi then []
   else lo :: range (lo + 1) hi
 
+let print_list collection =
+  List.iter (Printf.printf "%i") collection |> print_newline
+
+let u = [1; 2; 3; 4]
+
+let rec len collection =
+  match collection with
+  | [] -> 0
+  | _ :: i -> 1 + len i
+
 let () =
   square 50 |> string_of_int |> print_endline;
   cat "Hello," "World!" |> print_endline;
   print_endline "Hello, World!";
-  List.iter (Printf.printf "%d") (range 2 5);
-  print_newline()
+  print_list (range 2 5);
+  print_list u;
+
+  let u_size = len u in
+  Printf.printf "Size of 'u' is %d\n" u_size;
+
+  print_string "Your input: ";
+  let user_input =
+    read_line() in
+    print_endline ("User input: " ^ user_input);
+
+  let a = fun i -> i * i
+  in
+  print_endline ("A = " ^ (Int.to_string (a 50)))
